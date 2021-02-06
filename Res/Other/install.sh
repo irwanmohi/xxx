e3ea1ded0a25bb01af99d95f3f236de7387597d2
lanundarat87 <lanundarat87@users.noreply.github.com>
2021-02-06 08:50:07 +0800

    Update install.sh

--- Res/Other/install.sh
+++ Res/Other/install.sh
@@ -42,13 +42,6 @@ fi
 
 clear
 
-# Adding VPN Users
-echo "Set username:"
-read username
-echo "Set Password:"
-read password
-sudo echo "$username * $password *" >> /etc/ppp/chap-secrets
-
 # Restarting Service 
 sudo service pptpd restart
 
